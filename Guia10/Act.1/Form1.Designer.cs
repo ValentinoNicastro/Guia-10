4632ab7ee3226bfdf6a7399e1ea406b58a3181f3
ValentinoNicastro <valennicastro321@gmail.com>
2024-06-04 21:11:41 -0300

    Cambios

--- Guia10/Act.1/Form1.Designer.cs
+++ Guia10/Act.1/Form1.Designer.cs
@@ -28,13 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.tbNumero = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btListar = new System.Windows.Forms.Button();
+            this.lbLista = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btAgregar);
+            this.groupBox1.Controls.Add(this.tbNumero);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese Numero";
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(256, 36);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(75, 35);
+            this.btAgregar.TabIndex = 1;
+            this.btAgregar.Text = "Agregar Numero";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            this.btAgregar.Click += new System.EventHandler(this.btAgregar_Click);
+            // 
+            // tbNumero
+            // 
+            this.tbNumero.Location = new System.Drawing.Point(36, 44);
+            this.tbNumero.Name = "tbNumero";
+            this.tbNumero.Size = new System.Drawing.Size(132, 20);
+            this.tbNumero.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(13, 120);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(360, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mostrar Promedio";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton2);
+            this.groupBox3.Controls.Add(this.radioButton1);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Location = new System.Drawing.Point(13, 237);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(360, 166);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Busqueda De Numero";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(36, 115);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(36, 92);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(256, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 38);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Búsqueda";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lbLista);
+            this.groupBox4.Controls.Add(this.btListar);
+            this.groupBox4.Location = new System.Drawing.Point(379, 13);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(229, 390);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Lista Ordenada";
+            // 
+            // btListar
+            // 
+            this.btListar.Location = new System.Drawing.Point(135, 20);
+            this.btListar.Name = "btListar";
+            this.btListar.Size = new System.Drawing.Size(75, 44);
+            this.btListar.TabIndex = 0;
+            this.btListar.Text = "Listar";
+            this.btListar.UseVisualStyleBackColor = true;
+            this.btListar.Click += new System.EventHandler(this.btListar_Click);
+            // 
+            // lbLista
+            // 
+            this.lbLista.FormattingEnabled = true;
+            this.lbLista.Location = new System.Drawing.Point(7, 20);
+            this.lbLista.Name = "lbLista";
+            this.lbLista.Size = new System.Drawing.Size(120, 355);
+            this.lbLista.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(615, 410);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.TextBox tbNumero;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btListar;
+        private System.Windows.Forms.ListBox lbLista;
     }
 }
 
--- Guia10/Act.1/Form1.cs
+++ Guia10/Act.1/Form1.cs
@@ -16,5 +16,45 @@ namespace Act._1
         {
             InitializeComponent();
         }
+
+        int[] lista = new int[100];
+        int cont = 0;
+
+        private void btAgregar_Click(object sender, EventArgs e)
+        {
+            lista[cont] = Convert.ToInt32(tbNumero.Text);
+            cont++;
+            tbNumero.Clear();
+        }
+
+        public void ordenar()
+        {
+            int guardar;
+
+            for (int i = 0; i < cont - 1; i++)
+            {
+                for (int n = i + 1; n < cont; n++)
+                {
+                    if (i > j)
+                    {
+                        guardar = lista[i];
+                        lista[i] = lista[n];
+                        lista[n] = guardar;
+                    }
+                }
+            }
+        }
+
+        private void btListar_Click(object sender, EventArgs e)
+        {
+            lbLista.Items.Clear();
+
+            ordenar();
+
+            for (int i = 0; i < cont;i++)
+            {
+                lbLista.Items.Add(lista[i]);
+            }
+        }
     }
 }
